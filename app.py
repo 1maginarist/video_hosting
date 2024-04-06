84e45e9a6409a307a23ed973c2f9a1ac236dd449
Nikita Pomerantsev <NBHD20dhbn23>
2024-04-06 14:21:08 +0400

    second commit

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 from flask import Flask
 from waitress import serve
 from prometheus_flask_exporter import PrometheusMetrics
-import time
 
 # initializing app instance and metrics collector
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
 
 
-@app.route('/count/<x>')
-def count_route(x):
+@app.route('/hello')
+def count_route():
 
-    return
+    return {'body': 'hello Ilya'}
 
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=8082)
+    serve(app, host='0.0.0.0')
